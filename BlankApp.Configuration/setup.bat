5c8e6b20647882ebc5f5e0776ed19515d1367c47
“harfol” <1148706823@qq.com>
2022-01-20 17:33:10 +0800

    add .reg .bat

---
+++ BlankApp.Configuration/setup.bat
@@ -0,0 +1,8 @@
+﻿::添加环境变量 BAT_HOME
+@echo off
+echo 添加 bat 环境变量
+set regpath=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
+set evname=BAT_HOME
+reg add "%regpath%" /v %evname% /d %cd% /f
+regedit /s setup.reg
+pause>nul
